ddaf16e92044c5dc4bff033ee14d1bc47fa92ae7
Andrey Shevchenko <rainder2207@gmail.com>
2020-04-14 21:33:44 +0300

    delete 2 DZ

--- price_lesson6.py
+++ price_lesson6.py
@@ -4,9 +4,3 @@ def get_discount(one, two, delimiter = '&') :
 a = get_discount(1, 'Pyton')
 print(a.upper())
 
-def format_price(price):
-    price = int(price)
-    price_see = f'Price: {price} rub.'
-    return price_see
-b = format_price(56.24)
-print(b)
